11c5871fdb826c2511b39e2f98ccd8c677b71a95
KarulinAV <aw.karulin@gmail.com>
2022-04-14 00:21:10 +0300

    RESTful API

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+const articlesSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articlesSchema);
+
+app.route("/articles/:article")
+
+.get(function(req, res){
+  Article.findOne({title: req.params.article}, function(err, foundArticle){
+    if(!err){
+      res.send(foundArticle);
+    }else{
+      res.send(err);
+    }
+  })
+})
+
+app.route("/articles")
+
+.get(
+  function(req, res){
+    Article.find(function(err, foundArticles){
+      if(!err){
+        res.send(foundArticles);
+      }else{
+        res.send(err);
+      }
+    });
+  }
+)
+
+.post(function(req, res){
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  })
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfully added a new article.");
+    }else {
+      res.send(err);
+    }
+  })
+})
+
+.delete(function(req, res){
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("Successfully deleted all articles");
+    }else{
+      res.send(err);
+    }
+  })
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
